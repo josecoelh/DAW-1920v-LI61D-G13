7d1f98749eebafca2657918872f6c43ecb95380f
Jose Coelho <jose.coelho2@gmail.com>
2020-04-29 12:56:41 +0100

    pagination

--- extra/CREATE.sql
+++ extra/CREATE.sql
@@ -22,15 +22,16 @@ CREATE TABLE PROJECT(
 );
 
 CREATE TABLE PROJECT_USERS(
-	project_id UUID REFERENCES PROJECT(proj_id),
-	user_name VARCHAR(100) REFERENCES USERS(username),
-	PRIMARY KEY(project_id, user_name)
+	user_name VARCHAR(50) ,
+	project_id UUID,
+	PRIMARY KEY(project_id, user_name),
+	FOREIGN KEY (project_id) REFERENCES PROJECT(proj_id),
+	FOREIGN KEY (user_name) REFERENCES USERS(username)
 );
 
 CREATE TABLE ALLOWED_LABELS(
 	_value VARCHAR(50),
-	proj_id UUID,
-	FOREIGN KEY (proj_id) REFERENCES PROJECT(proj_id),
+	proj_id UUID REFERENCES PROJECT(proj_id),
 	PRIMARY KEY(_value, proj_id)
 );
 
@@ -55,7 +56,6 @@ CREATE TABLE _COMMENTS(
 	_DATE VARCHAR(16),
 	comment_id UUID,
 	issue_id UUID,
-	username VARCHAR(50) REFERENCES USERS(username),
 	FOREIGN KEY (issue_id) REFERENCES ISSUES(issue_id),
 	PRIMARY KEY(comment_id)
 );
@@ -63,7 +63,7 @@ CREATE TABLE _COMMENTS(
 /* INSERT TEST VALUES*/
 insert  into  USERS values ('dXNlcm5hbWU6cGFzc3dvcmQ=','username');
 insert  into  project values ('3779a41b-7920-4f9c-b3aa-75d9302a1abe','cena do ','teste fudido');
-INSERT INTO PROJECT_USERS VALUES('3779a41b-7920-4f9c-b3aa-75d9302a1abe','username');
+insert into project_users values ('username','3779a41b-7920-4f9c-b3aa-75d9302a1abe');
 insert into allowed_labels values ('v','3779a41b-7920-4f9c-b3aa-75d9302a1abe');
 insert into allowed_labels values ('vv','3779a41b-7920-4f9c-b3aa-75d9302a1abe');
 insert into issues values ('issueTest', '3779a41b-7920-4f9c-b3aa-75d9302a2abe','3779a41b-7920-4f9c-b3aa-75d9302a1abe','CLOSED');
@@ -72,10 +72,8 @@ insert into _comments values ('teste','04/02/2020 04 03','9611db05-b2eb-449d-87d
 insert into _comments values ('teste','04/02/2020 04 03','9611db05-b2eb-449d-87dc-427bf8f3944a','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
 insert into issue_labels values ('v','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
 insert into issue_labels values ('vv','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
-abe');
-insert into _comments values ('teste','04/02/2020 04 03','9611db05-b2eb-449d-87dc-427bf8f3944f','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
-insert into _comments values ('teste','04/02/2020 04 03','9611db05-b2eb-449d-87dc-427bf8f3944a','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
-insert into issue_labels values ('v','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
-insert into issue_labels values ('vv','3779a41b-7920-4f9c-b3aa-75d9302a2abe');
 
+select * from project_users inner join project on (project_id = proj_id)
+--SELECT * FROM project WHERE proj_id  in (SELECT project_id from PROJECT_USERS where user_name = 'test') order by _name limit 10 offset 1
+SELECT * FROM project WHERE proj_id  in (SELECT project_id from PROJECT_USERS where user_name = 'username') order by _name limit 10 offset 0
 
