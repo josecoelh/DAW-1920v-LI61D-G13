954358e0a69b16965c21f81b486121701bff9a52
Jose Coelho <jose.coelho2@gmail.com>
2020-05-02 13:18:21 +0100

    hook usage

--- react-client/src/App.jsx
+++ react-client/src/App.jsx
@@ -1,63 +1,59 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import './App.scss';
 import  { Login, Register } from "./components/login/index"
 
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      isLogginActive : true
-    }
-  }
+function App(){
+  let isLogginActive = null;
+  useEffect(()=>{
+    isLogginActive = true;
+  }, [])
+  const sideRef = useRef(null);
+  const current = isLogginActive? "Register" : "Login";
+  const currentActive = isLogginActive?  "Login" :"Register" ;
 
-  changeState(){
-    const {isLogginActive} = this.state;
-    if (isLogginActive) {
-      this.side.classList.remove("right");
-      this.side.classList.add("left");
-    } else {
-      this.side.classList.remove("left");
-      this.side.classList.add("right");
-    }
-    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive })); 
-  }
-
-  render(){
-    const {isLogginActive} = this.state;
-    const current = isLogginActive? "Register" : "Login";
-    const currentActive = isLogginActive?  "Login" :"Register" ;
-
+  const Side = props => {
     return (
+      <div
+        className="side"
+        ref={sideRef}
+        onClick={props.onClick}
+      >
+        <div className="inner-container">
+          <div className="text"> {props.current} </div>
+        </div>
+      </div>
+    );
+  };
+  
+  const sideClick = function changeState(){
+    if (isLogginActive) {
+      sideRef.current.classList.remove("right");
+      sideRef.current.classList.add("left");
+    } else {
+      sideRef.current.classList.remove("left");
+      sideRef.current.classList.add("right");
+    }
+    isLogginActive = !isLogginActive 
+  }
+
+  return(
       <div className="App">
         <div className="login">
-          <div className="container" ref={ref => (this.container = ref)}>
+          <div className="container" >
             {isLogginActive && <Login  />}
             {!isLogginActive && <Register />}
           </div>
           <Side
             current={current}
-            currentActive={currentActive}
-            containerRef={ref => (this.side = ref)}
-            onClick={this.changeState.bind(this)}
+            onClick={sideClick.bind(this)}
           />
         </div>
       </div>
     );
-  }
+
 }
 
-const Side = props => {
-  return (
-    <div
-      className="side"
-      ref={props.containerRef}
-      onClick={props.onClick}
-    >
-      <div className="inner-container">
-        <div className="text"> {props.current} </div>
-      </div>
-    </div>
-  );
-};
+
+
 
 export default App;
